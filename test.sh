4c4adfa9159b2d89634cd2445bc0a9b1614cf159
Žiga Leber <mail+github@zigaleber.com>
2024-02-21 12:09:14 +0100

    Add more examples

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ declare -r path=$(dirname $(realpath "$0"))
 
 declare -i err=0
 
-for file in "$path"/examples/example{0..0}.txt
+for file in "$path"/examples/example{0..5}.txt
 do
     echo "$file"
     cat "$file"
